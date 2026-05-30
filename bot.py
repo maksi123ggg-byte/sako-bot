cd5a6bec381cb872b76c680e1b49b91920fb55c5
maksi123ggg-byte <maksi123ggg@gmail.com>
2026-05-30 03:56:21 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,13 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-# ВАШ ТОКЕН ИЗ BOTFATHER (Убедитесь, что он рабочий и скопирован без ошибок!)
+# Актуальный токен из вашего BotFather
 TOKEN = "8586142798:AAEJ3iqff4TnmqM19e-encZphrJb9G_fC0M"
 
-# ВАШИ ЛИЧНЫЕ ПРОФИЛИ:
+# Ваши личные профили и контакты
 FUNPAY_URL = "https://funpay.com/uk/users/19612186/"
 PAYGAME_URL = "https://paygame.ru/users/SAKO1"
-REVIEWS_URL = "https://funpay.com/uk/users/19612186/"
+REVIEWS_URL = "https://funpay.com/uk/users/19612186/
 SUPPORT_URL = "t.me/SK_SAKO"
 
 bot = Bot(token=TOKEN)
